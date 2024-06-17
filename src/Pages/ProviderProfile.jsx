12ec029f5a1ba1bae904b8898f0c0ceaeef91ff8
unknown <promiseokpoto5050@gmail.com>
2024-06-17 17:08:24 +0100

    my changes

--- src/Pages/ProviderProfile.jsx
+++ src/Pages/ProviderProfile.jsx
@@ -823,8 +823,6 @@ async function changeImage(e){
                 <button onClick={() => setFields("about")} className={fields==="about" ? "active-fields" : ""}>About me</button>
                 <button onClick={() => setFields("socials")} className={fields==="socials" ? "active-fields" : ""}>Social links</button>
                 <button onClick={() => setFields("password")} className={fields==="password" ? "active-fields" : ""}>Change password</button>
-                {/* <button>{providerId}</button> */}
-                {/* <button>{oldPhone2}</button> */}
             </div>
 
             <form className={fields==="password" ? "hide-field" : "edit"}>
